cdda12d87b2bfc2e0b5e08258124dbe3b8e3ffa9
Phan Anh Tú <124888378+tuphan22028238@users.noreply.github.com>
2023-11-01 14:15:05 +0700

    Week 2

---
+++ Week2/HelloWorld.java
@@ -0,0 +1,12 @@
+// Import your library
+// Do not change the name of the Solution class
+public class HelloWorld {
+    // Type your main code here
+    static String printHello() {
+        return "HelloWorld";
+    }
+    
+    public static void main(String[] args) {
+        System.out.print(printHello());
+    }
+} 
